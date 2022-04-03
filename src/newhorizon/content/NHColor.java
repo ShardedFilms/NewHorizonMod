4959b4bb857618e478d1141131df9d1d32475b37
Sharded <98332856+ShardedFilms@users.noreply.github.com>
2022-04-03 16:29:22 +0700

    Update NHColor.java

--- src/newhorizon/content/NHColor.java
+++ src/newhorizon/content/NHColor.java
@@ -16,13 +16,13 @@ public class NHColor{
 		darkEnr = darkEnrColor.cpy().lerp(Color.black, 0.85f),
 		darkEnrFront = darkEnrColor.cpy().lerp(Color.white, 0.2f),
 		trail = Color.lightGray.cpy().lerp(Color.gray, 0.65f),
-		thermoPst = Color.valueOf("CFFF87").lerp(Color.white, 0.15f);
+		thermoPst = Color.valueOf("CFFF87").lerp(Color.white, 0.15f),
 	
 		//Exogenesis are by Sharded... right?
 		
 		cyanBright = Color.valueOf("#99F8FF"),
 		cyanMed = Color.valueOf("#8A8E99"),
-		cyanLow = Color.valueOf("6A97D9"),
+		cyanLow = Color.valueOf("6A97D9");
 	
 		//How this i made.. well was
 	static{
